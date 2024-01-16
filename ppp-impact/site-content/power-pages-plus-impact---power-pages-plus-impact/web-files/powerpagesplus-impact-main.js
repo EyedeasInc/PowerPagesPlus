dd5d6223600cbbc2c4e5abad0046c8afd583794f
Victor Dantas <dantas_victor@hotmail.com>
2024-01-16 01:34:59 -0700

    Adding Power Pages Plus Impact site

---
+++ ppp-impact/site-content/power-pages-plus-impact---power-pages-plus-impact/web-files/powerpagesplus-impact-main.js
@@ -0,0 +1,17 @@
+/**
+* Template Name: Impact
+* Updated: Sep 18 2023 with Bootstrap v5.3.2
+* Template URL: https://bootstrapmade.com/impact-bootstrap-business-website-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+    /**
+     * Initiate glightbox
+     */
+    const glightbox = GLightbox({
+        selector: '.glightbox'
+    });
+
+});
