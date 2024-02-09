c77a0128bc45fc178eb6934b50f2a2e772e2706b
Victor Dantas <dantas_victor@hotmail.com>
2024-02-09 12:03:48 -0700

    Adding Minimalist template

---
+++ ppp-minimalist/site-content/power-pages-plus-minimalist---power-pages-plus-minimalist/web-files/powerpagesplus-main.js
@@ -0,0 +1,11 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+    /**
+     * Initiate glightbox
+     */
+    const glightbox = GLightbox({
+        selector: '.glightbox'
+    });
+
+});
